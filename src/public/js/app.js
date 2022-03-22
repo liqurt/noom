0549b409f5ca6fd43bd2095fd42302a052ae2273
윤승일(Seung-Il Yoon) <liqurtJob@gmail.com>
2022-03-22 14:17:48 +0900

    Json <-> String 전환하면서 메시지 전달, 전달받은 메시지 타입 관계없이 출력

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,14 +4,17 @@
 const socket = new WebSocket(`ws://${window.location.host}`)
 
 const messageList = document.querySelector("ul")
-const messageForm = document.querySelector("form")
+const messageForm = document.querySelector("#message")
+const nickForm = document.querySelector("#nick")
 
 socket.addEventListener("open", ()=>{
     console.log("Connected to Server ✅")
 })
 
 socket.addEventListener("message", (message) =>{
-    console.log("New message: ", message.data)
+    const li = document.createElement("li")
+    li.innerText = message.data
+    messageList.append(li)
 })
 
 socket.addEventListener("close", () => {
@@ -25,7 +28,21 @@ socket.addEventListener("close", () => {
 function handleSubmit(event){
     event.preventDefault()
     const input = messageForm.querySelector("input")
-    socket.send(input.value)
+    socket.send(makeMessage("new_message",input.value))
     input.value = ""
 }
+
+function handleNickSubmit(event){
+    event.preventDefault()
+    const input = nickForm.querySelector("input")
+    // text가 아니라 Json으로 서버에 전송
+    socket.send(makeMessage("nickname",input.value))
+}
+
+function makeMessage(type, payload){
+    const msg = {type, payload}
+    return JSON.stringify(msg)
+}
+
 messageForm.addEventListener("submit", handleSubmit)
+nickForm.addEventListener("submit", handleNickSubmit)
